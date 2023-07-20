693a674dba26456e7b96bbfbb9dff7e062351860
DIPAN Roy Chowdhury <dipan.roy.chowdhury@ibm.com>
2023-07-20 20:50:27 +0530

    Modified GenerateRandomNumber.java

--- src/main/java/org/example/GenerateRandomNumber.java
+++ src/main/java/org/example/GenerateRandomNumber.java
@@ -1,21 +1,26 @@
 package org.example;
 
+import java.util.ArrayList;
 import java.util.List;
-import java.util.Random;
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
 
 public class GenerateRandomNumber {
-
     public List<Integer> generateRandomNumbers(){
-        Random random = new Random();
-        int randomNum =  random.nextInt(200);
+        int min = 1; // Minimum value of range
+        int max = 100; // Maximum value of range
+        int randomNumber = -1;
+        List<Integer> randoms = new ArrayList<Integer>();
+        // Print the min and max
+        System.out.println("Random value in int from "+ min + " to " + max + ":");
+        // Generate random int value from min to max
+        for (int i = 1; i <= 2000; i++) {
+            randomNumber = (int) Math.floor(Math.random() * (max - min + 1) + min);
+            // Printing the generated random numbers
+//            System.out.println("Random Numbers :: "+randomNumber);
+            randoms.add(randomNumber);
+        }
 
-        List<Integer> randoms = random.ints(randomNum)
-                .boxed()
-                .collect(Collectors.toList());
-
-        System.out.println("Random Numbers :: "+randoms);
+        System.out.println(randoms);
+        System.out.println(randoms.size());
         return randoms;
     }
 
